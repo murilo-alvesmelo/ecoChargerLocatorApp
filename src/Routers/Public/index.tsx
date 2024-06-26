4cd6685ce1a31991e7fa8204b19e0c0967556ac1
Murilo Alves Melo <murilo.melo72@gmail.com>
2024-06-25 21:40:11 -0300

    chore: add private and public routes

--- src/Routers/Public/index.tsx
+++ src/Routers/Public/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import InitialScreen from '../../Screens/InitialScreen';
 import LoginOptionsScreen from '../../Screens/LoginOptionsScreen';
+import Login from '../../Screens/Login';
+import CadastroScreen from '../../Screens/CadastroScreen';
 
 const Stack = createStackNavigator();
 
-export default function StackRouter() {
+export default function PublicStack() {
   return (
     <Stack.Navigator
       initialRouteName="InitialScreen"
@@ -21,6 +23,12 @@ export default function StackRouter() {
         name="LoginOptionsScreen"
         component={LoginOptionsScreen}
         options={{title: 'LoginOptionsScreen'}}
+      />
+      <Stack.Screen name="Login" component={Login} options={{title: 'Login'}} />
+      <Stack.Screen
+        name="Register"
+        component={CadastroScreen}
+        options={{title: 'Register'}}
       />
     </Stack.Navigator>
   );
