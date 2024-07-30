445c494fce7a9d182936e92652408733370614ca
Murilo Alves Melo <murilo.melo72@gmail.com>
2024-07-29 21:49:44 -0300

    chore: ajuste animacao banner

--- src/Screens/HomeScreen/style.ts
+++ src/Screens/HomeScreen/style.ts
@@ -1,5 +1,18 @@
 import {StyleSheet} from 'react-native';
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  mapView: {
+    flex: 1,
+    zIndex: 0,
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+});
 
 export default style;
